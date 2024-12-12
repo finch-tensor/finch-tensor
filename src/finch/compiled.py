ed5e3b472506febdcc9a5a76d2f2f4e757b24578
Hameer Abbasi <2190658+hameerabbasi@users.noreply.github.com>
2024-12-12 09:28:23 +0100

    Materialize only in outermost nested `@compiled`.

--- src/finch/compiled.py
+++ src/finch/compiled.py
@@ -4,20 +4,53 @@ from .julia import jl
 from .tensor import Tensor
 
 
-def compiled(opt=None):
+def _recurse(x, /, *, f):
+    if isinstance(x, tuple | list):
+        return type(x)(_recurse(xi) for xi in x)
+    if isinstance(x, dict):
+        return {k: _recurse(v) for k, v in x}
+    return f(x)
+
+
+def _recurse_iter(x, /):
+    if isinstance(x, tuple | list):
+        yield from (_recurse(xi) for xi in x)
+    if isinstance(x, dict):
+        yield from (_recurse(xi) for xi in x.values())
+    yield x
+
+
+def _to_lazy_tensor(x, /):
+    if isinstance(x, Tensor) and not jl.isa(x._obj, jl.Finch.LazyTensor):
+        return Tensor(jl.Finch.LazyTensor(x._obj))
+
+    return x
+
+
+def compiled(opt=None, force_materialization=False):
     def inner(func):
         @wraps(func)
         def wrapper_func(*args, **kwargs):
-            new_args = []
-            for arg in args:
-                if isinstance(arg, Tensor) and not jl.isa(arg._obj, jl.Finch.LazyTensor):
-                    new_args.append(Tensor(jl.Finch.LazyTensor(arg._obj)))
-                else:  
-                    new_args.append(arg)
-            result = func(*new_args, **kwargs)
-            kwargs = {"ctx": opt.get_julia_scheduler()} if opt is not None else {}
-            result_tensor = Tensor(jl.Finch.compute(result._obj, **kwargs))
+            args = tuple(args)
+            kwargs = dict(kwargs)
+            compute_at_end = True
+            if not force_materialization and any(
+                isinstance(arg, Tensor) and jl.isa(arg._obj, jl.Finch.LazyTensor)
+                for arg in _recurse_iter((args, kwargs))
+            ):
+                compute_at_end = False
+
+            args = _recurse(args, f=_to_lazy_tensor)
+            kwargs = _recurse(kwargs, f=_to_lazy_tensor)
+            result = func(*args, **kwargs)
+            if not compute_at_end:
+                return result
+            compute_kwargs = (
+                {"ctx": opt.get_julia_scheduler()} if opt is not None else {}
+            )
+            result_tensor = Tensor(jl.Finch.compute(result._obj, **compute_kwargs))
             return result_tensor
+
         return wrapper_func
 
     return inner
