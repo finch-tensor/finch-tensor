2c35f13e886cc4eda478549e79a80f15a3c92a96
Willow Ahrens <willow@csail.mit.edu>
2025-02-28 15:46:25 -0500

    fix

--- src/finch/julia.py
+++ src/finch/julia.py
@@ -7,4 +7,7 @@ import juliacall as jc  # noqa
 from juliacall import Main as jl  # noqa
 
 jl.seval("using Finch")
+jl.seval("using HDF5")
+jl.seval("using NPZ")
+jl.seval("using TensorMarket")
 jl.seval("using Random")
