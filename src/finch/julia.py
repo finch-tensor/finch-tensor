5840b05b67aa5518170626dbd7be0eea670a4e53
Willow Ahrens <willow@csail.mit.edu>
2025-02-28 14:19:00 -0500

    fix julia package versioning, bump finch/juliapkg

--- src/finch/julia.py
+++ src/finch/julia.py
@@ -1,36 +1,9 @@
 import os
 
-import juliapkg
-
-
-def add_package(name: str, hash: str, version: str) -> None:
-    deps = juliapkg.deps.load_cur_deps()
-    if deps.get("packages", {}).get(name, {}).get("version", None) != version:
-        juliapkg.add(name, hash, version=version)
-        juliapkg.resolve()
-
-
-_FINCH_NAME = "Finch"
-_FINCH_VERSION = "1.2.0"
-_FINCH_HASH = "9177782c-1635-4eb9-9bfb-d9dfa25e6bce"
-_FINCH_REPO_PATH = os.environ.get("FINCH_REPO_PATH", default=None)
-_FINCH_REPO_URL = os.environ.get("FINCH_URL_PATH", default=None)
-
-if _FINCH_REPO_PATH and _FINCH_REPO_URL:
-    raise ValueError(
-        "FINCH_REPO_PATH and FINCH_URL_PATH can't be set at the same time."
-    )
-
-if _FINCH_REPO_PATH:  # Also account for empty string
-    juliapkg.add(_FINCH_NAME, _FINCH_HASH, path=_FINCH_REPO_PATH, dev=True)
-elif _FINCH_REPO_URL:
-    juliapkg.add(_FINCH_NAME, _FINCH_HASH, url=_FINCH_REPO_URL, dev=True)
-else:
-    add_package(_FINCH_NAME, _FINCH_HASH, _FINCH_VERSION)
-
+#To change the version of Finch used, see the documentation for pyjuliapkg here: https://github.com/JuliaPy/pyjuliapkg
+#Use pyjuliapkg to modify the `juliapkg.json` file in the root of this repo.
+#An example development json is found in `juliapkg_dev.json`
 import juliacall as jc  # noqa
-
-juliapkg.resolve()
 from juliacall import Main as jl  # noqa
 
 jl.seval("using Finch")
