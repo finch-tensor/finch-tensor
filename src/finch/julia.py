2b4ce699ae623cf4dfe92b6bd545d44e96e2a668
Mateusz Sokół <mat646@gmail.com>
2025-03-04 19:06:55 +0000

    Reinstate `juliapkg.resolve` call

--- src/finch/julia.py
+++ src/finch/julia.py
@@ -1,9 +1,11 @@
-import os
-
+import juliapkg
 #To change the version of Finch used, see the documentation for pyjuliapkg here: https://github.com/JuliaPy/pyjuliapkg
 #Use pyjuliapkg to modify the `juliapkg.json` file in the root of this repo.
 #An example development json is found in `juliapkg_dev.json`
 import juliacall as jc  # noqa
+
+juliapkg.resolve()
+
 from juliacall import Main as jl  # noqa
 
 jl.seval("using Finch")
