bd026b068bd229af51520f57a9e4e6b51d7cd9e6
Mateusz Sokół <mat646@gmail.com>
2025-04-29 12:46:38 +0000

    Upgrade Array API to 2024.12

--- ci/test_array_api.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -euxo pipefail
-
-source ci/clone_array_api_tests.sh
-
-python -c 'import finch'
-ARRAY_API_TESTS_MODULE="finch" pytest "$ARRAY_API_TESTS_DIR/array_api_tests/" -v -c "$ARRAY_API_TESTS_DIR/pytest.ini" --ci --max-examples=2 --derandomize --disable-deadline --disable-warnings -n auto --skips-file ci/array-api-skips.txt
--- src/finch/tensor.py
+++ src/finch/tensor.py
@@ -645,9 +645,9 @@ class Tensor(_Display, SparseArray):
 
     def __array_namespace__(self, *, api_version: str | None = None) -> Any:
         if api_version is None:
-            api_version = "2023.12"
+            api_version = "2024.12"
 
-        if api_version not in {"2021.12", "2022.12", "2023.12"}:
+        if api_version not in {"2021.12", "2022.12", "2023.12", "2024.12"}:
             raise ValueError(f'"{api_version}" Array API version not supported.')
         import finch
 
