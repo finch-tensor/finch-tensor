48b6b902307f54459b8846a7823ee41add868347
JoelMathewC <joelmathewcherian@gmail.com>
2026-02-10 20:32:43 -0500

    feat: improved the compiler interface

--- src/finch/compiler.py
+++ src/finch/compiler.py
@@ -1,31 +1,42 @@
-import finchlite
+from .tensor import FinchJLTensor
+from finchlite import NotationCompiler, AssemblyKernel, AssemblyLibrary
+import finchlite.finch_notation.nodes as ntn
 
-from finchlite import (
-    Loop,
-    Variable,
-    Index,
-    NotationStatement,
-    NotationModule,
-)
+from juliacall import Main as jl 
 
-class FinchJLKernel(finchlite.AssemblyKernel):
-    def __call__(self, *args: FinchJLTensor...) -> tuple[FinchJLTensor...]:
-        ...
-    
-class FinchJLLibrary(finchlite.AssemblyLibrary):
-    kernels: dict[str, FinchJLKernel]
-    def getattr(self, name: str) -> FinchJLKernel:
+CompiledModuleName = "compiled_module"
+FunctionName = "compiled_function"
+
+class FinchJLKernel(AssemblyKernel):
+    def __init__(self, func_name, jl_code): 
+        self.func_name = func_name
+        jl.seval(jl_code)
+    def __call__(
+        self, *args: tuple[FinchJLTensor, ...]
+    ) -> tuple[FinchJLTensor, ...]:
+        argList = []       
+        for arg in args:
+            argList.append(f"arg{len(argList)}")
+            setattr(jl, argList[-1], arg)
+
+        jl.seval(f"{self.func_name}({argList.join(',')})")
+
+class FinchJLLibrary(AssemblyLibrary):
+    def __init__(self, kernel_name, kernel):
+        self.kernel_dict = {kernel_name: kernel}
+
+    def __getattr__(self, name: str) -> FinchJLKernel:
         return self.kernels[name]
 
-class FinchJLGenerator:
 
-    def __call__(self, prgm: NotationModule) -> FinchJLLibrary:
+class FinchJLGenerator:
+    def __call__(self, prgm: ntn.Module) -> FinchJLLibrary:
         match prgm:
-            case NotationModule(statements=stmts):
+            case ntn.Module(statements=stmts):
                 ...
 
-class FinchJLCompiler(finchlite.NotationCompiler):
-    def __call__(self, prgm:NotationModule) -> finchlite.FinchJLLibrary:
+class FinchJLCompiler(NotationCompiler):
+    def __call__(self, prgm: ntn.Module) -> FinchJLLibrary:
         generator = FinchJLGenerator()
         jl_code = generator(prgm)
-        return eval(jl_code)
+        return FinchJLLibrary(CompiledModuleName, FinchJLKernel(FunctionName, jl_code))
