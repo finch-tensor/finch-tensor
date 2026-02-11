22b2f71bc6bf782b9b1c836066b6ee772fd2fc41
JoelMathewC <joelmathewcherian@gmail.com>
2026-02-10 23:41:55 -0500

    wip: added match cases for most finch-tensor-lite finch-ntn nodes

--- src/finch/compiler.py
+++ src/finch/compiler.py
@@ -1,9 +1,14 @@
 from finchlite.compile import NotationCompiler
 from finchlite.finch_assembly import AssemblyKernel, AssemblyLibrary
 import finchlite.finch_notation.nodes as ntn
+from finchlite.compile import dimension
 from typing import Any
 
+import operator
+
 from juliacall import Main as jl
+
+ops_map = {operator.add: "+", operator.mul: "*"}
 
 
 class FinchJLKernel(AssemblyKernel):
@@ -31,61 +36,113 @@ class FinchJLLibrary(AssemblyLibrary):
 
 
 class FinchJLGenerator:
+    def __init__(self):
+        self.pack_dict = {}
+
     def __call__(self, prgm: ntn.Module) -> FinchJLLibrary:
+        self.pack_dict.clear()
+        return self.generate_julia(prgm)
+
+    def generate_julia(self, prgm, nestingLvl=0):
         match prgm:
-            case ntn.Literal(val):
-                ...
-            # What this?
-            case ntn.Value(ex, type_):
-                ...
-            case ntn.Variable(name, type_):
-                ...
-            case ntn.Call(op, args):
-                ...
-            case ntn.Dimension(tns, r):
-                ...
-            case ntn.Access(tns, mode, idxs):
-                ...
-            case ntn.Read():
-                ...
-            case ntn.AccessMode():
-                ...
-            case ntn.Update(op):
-                ...
-            case ntn.Increment(lhs,rhs):
-                ...
-            case ntn.Unwrap(arg):
-                ...
-            case ntn.Cached(arg,ref):
-                ...
-            case ntn.Loop(idx, ext, body):
-                ...
-            case ntn.If(cond, body):
-                ...
-            case ntn.IfElse(cond, then_body, else_body):
-                ...
-            case ntn.Assign(lhs,rhs):
-                ...
-            case ntn.Stack(obj,type):
-                ...
-            case ntn.Slot(name, type):
-                ...
-            case ntn.Unpack(lhs, rhs):
-                ...
-            case ntn.Repack(val, obj):
-                ...
-            case ntn.Declare(tns, init, op, shape):
-                ...
-            case ntn.Freeze(tns, op):
-                ...
-            case ntn.Thaw(tns, op):
-                ...
+            case ntn.Function(name, args, body):
+                body_str = self.generate_julia(body, nestingLvl + 1)
+                return f"function {name}\n{body_str}\nend"
+
             case ntn.Block(bodies):
-                ...
-            case ntn.Function(name,args,body):
-                ...
+                body_str = ""
+                tab_str = {"    " * nestingLvl}
+                for body in bodies:
+                    body_str += f"{tab_str}{self.generate_julia(body, nestingLvl)}\n"
+
+            case ntn.Assign(lhs, rhs):
+                # TODO: Can we make this better?
+                # Special condition to ignore all assigns associated with
+                # finding loop bounds
+                if isinstance(rhs, ntn.Call) and rhs.op.val == dimension:
+                    return ""
+                return f"{self.generate_julia(lhs, nestingLvl)} = {self.generate_julia(body, nestingLvl)}"
+
+            case ntn.Declare(tns, init, op, shape):
+                # TODO: what is the purpose of op here
+                return f"@finch {self.generate_julia(tns, nestingLvl)} .= {self.generate_julia(init, nestingLvl)}"
+
             case ntn.Return(val):
-                ...
+                return f"return {self.generate_julia(val, nestingLvl)}"
+
+            case ntn.Loop(idx, _, body):
+                tab_str = "    " * nestingLvl
+                loop_body = self.generate_julia(body, nestingLvl + 1)
+                return f"for {idx.name} = _\n{loop_body}\n{tab_str}end"
+
+            case ntn.Access(tns, _, idxs):
+                tns_str = self.generate_julia(tns, nestingLvl)
+                idx_str = [self.generate_julia(idx, nestingLvl) for idx in idxs].join(
+                    ","
+                )
+                return f"{tns_str}[{idx_str}]"
+
+            case ntn.Call(op, args):
+                arg_str = [self.generate_julia(arg, nestingLvl) for arg in args].join(
+                    ","
+                )
+                return f"{ops_map[op.val]}({arg_str})"
+
+            case ntn.If(cond, body):
+                tab_str = "    " * nestingLvl
+                cond_str = self.generate_julia(cond, nestingLvl)
+                body_str = self.generate_julia(body, nestingLvl + 1)
+                return f"if {cond_str}\n{body_str}\n{tab_str}end"
+
+            case ntn.IfElse(cond, then_body, else_body):
+                cond_str = self.generate_julia(cond, nestingLvl)
+                then_body_str = self.generate_julia(then_body, nestingLvl + 1)
+                else_body_str = self.generate_julia(else_body, nestingLvl + 1)
+                return f"if {cond_str}\n{then_body_str}\n{tab_str}else\n{else_body_str}\n{tab_str}end"
+
+            case ntn.Increment(lhs, rhs):
+                lhs_str = self.generate_julia(lhs, nestingLvl)
+                rhs_str = self.generate_julia(rhs, nestingLvl)
+
+                # TODO: Is this the correct assumption to make
+                if not (
+                    isinstance(lhs, ntn.Access) and isinstance(lhs.mode, ntn.Update)
+                ):
+                    raise Exception("Increment expects the lhs to be an access")
+
+                return f"{lhs_str} {ops_map[lhs.mode.op.val]}= {rhs_str}"
+
+            case ntn.Unwrap(arg):
+                return self.generate_julia(arg, nestingLvl)
+
+            case ntn.Unpack(lhs, rhs):
+                # TODO: Is this the right assumption to make
+                if not isinstance(rhs, ntn.Variable):
+                    raise Exception("The unpack was not called with variable as RHS.")
+                self.pack_dict[lhs.name] = rhs.name
+                return ""
+
+            case ntn.Repack(val, _):
+                self.pack_dict.pop(val.name)
+                return ""
+
+            case ntn.Freeze(_, _):
+                return ""
+
+            case ntn.Slot(name):
+                if name not in self.pack_dict:
+                    raise Exception(f"{name} Slot does not exist in registry.")
+                return self.pack_dict[name]
+
+            case ntn.Literal(val):
+                return str(val)
+
+            case ntn.Variable(name, _):
+                return name
+
+            # TODO: Cached, Dimension, Thaw, Stack, Value are unimplemented.
+            case _:
+                raise Exception(f"Unhandled node type: {type(prgm)}")
 
 
 class FinchJLCompiler(NotationCompiler):
