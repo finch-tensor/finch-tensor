74a4b2397a561aa1202ddbce35e7efc65c673ecf
JoelMathewC <joelmathewcherian@gmail.com>
2026-03-17 11:41:30 -0400

    fix: broken import

--- src/finch/compiler.py
+++ src/finch/compiler.py
@@ -3,7 +3,7 @@ import operator
 
 import finchlite.finch_notation.nodes as ntn
 from finchlite.algebra import make_tuple
-from finchlite.algebra.operator import overwrite, promote_max, promote_min
+from finchlite.algebra.algebra import overwrite, promote_max, promote_min
 from finchlite.compile import NotationCompiler, dimension
 from finchlite.finch_assembly import AssemblyKernel, AssemblyLibrary
 
