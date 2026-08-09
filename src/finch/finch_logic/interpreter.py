493c13d251be686917caf806ecca94647e103ecf
kylebd99 <kylebd99@gmail.com>
2026-08-09 17:07:08 -0400

    simplify literal handling within interpreter

--- src/finch/finch_logic/interpreter.py
+++ src/finch/finch_logic/interpreter.py
@@ -12,6 +12,8 @@ from finch.symbolic import UnvalidatedForm
 from finch.util.logging import LOG_LOGIC_PRE_OPT
 
 from . import nodes as lgc
+from finch.tensor.scalar import Scalar
+
 from .nodes import (
     Aggregate,
     Alias,
@@ -62,7 +64,10 @@ class LogicMachine:
         logger.debug("Evaluating: %s", node)
         match node:
             case Literal(val):
-                return val
+                # A literal is a zero-dimensional tensor, so it evaluates to a
+                # rank-0 TableValue and every consumer below can treat it
+                # uniformly rather than special-casing raw scalars.
+                return TableValue(Scalar(val), ())
             case Value(_):
                 raise ValueError(
                     "The interpreter cannot evaluate a lazy node. Instead, you can "
@@ -87,8 +92,6 @@ class LogicMachine:
                 dims = {}
                 idxs = []
                 for arg in args:
-                    if not isinstance(arg, TableValue):
-                        continue
                     for idx, dim in zip(arg.idxs, arg.tns.shape, strict=True):
                         if idx in dims:
                             if dims[idx] != dim:
@@ -96,21 +99,8 @@ class LogicMachine:
                         else:
                             idxs.append(idx)
                             dims[idx] = dim
-                fill_val = op(
-                    *[
-                        arg.tns.fill_value if isinstance(arg, TableValue) else arg
-                        for arg in args
-                    ]
-                )
-                dtype = return_type(
-                    op,
-                    *[
-                        arg.tns.element_type
-                        if isinstance(arg, TableValue)
-                        else ftype(arg)
-                        for arg in args
-                    ],
-                )
+                fill_val = op(*[arg.tns.fill_value for arg in args])
+                dtype = return_type(op, *[arg.tns.element_type for arg in args])
                 result = self.make_tensor(
                     tuple(dims[idx] for idx in idxs), fill_val, dtype=dtype
                 )
@@ -118,8 +108,6 @@ class LogicMachine:
                     idx_crds = dict(zip(idxs, crds, strict=True))
                     vals = [
                         arg.tns[*[idx_crds[idx] for idx in arg.idxs]].item()
-                        if isinstance(arg, TableValue)
-                        else arg
                         for arg in args
                     ]
                     result[*crds] = op(*vals)
