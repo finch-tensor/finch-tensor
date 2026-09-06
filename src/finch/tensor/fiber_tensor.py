a776a7be284b8e3dc3ec6db5d68df81b0e35a1a9
JoelMathewC <joelmathewcherian@gmail.com>
2026-09-06 18:38:54 -0400

    feat: lower tensor item updates through lazy ops

--- src/finch/tensor/fiber_tensor.py
+++ src/finch/tensor/fiber_tensor.py
@@ -104,6 +104,7 @@ class FiberTensor(OverrideTensor):
         return self.lvl.buffer_factory
 
     def to_numpy(self) -> np.ndarray:
+<<<<<<< HEAD
         curr = self.lvl
         while hasattr(curr, "lvl"):
             curr = curr.lvl
@@ -111,6 +112,10 @@ class FiberTensor(OverrideTensor):
             buf = curr.val.arr if hasattr(curr.val, "arr") else curr.val
             return np.reshape(buf, self.shape)
         return self.to_scipy().toarray()
+=======
+        # TODO: temporary for dense only. TBD in sparse_level PR
+        return np.reshape(self.lvl.val.arr, self.shape, copy=False)
+>>>>>>> 7a87b59f (feat: lower tensor item updates through lazy ops)
 
     def to_scipy(self):
         from .level import (
