f3f28012b9aa691e431ae657b457795c4cc1b8f7
Joel Mathew Cherian <joelmathewcherian@gmail.com>
2026-09-09 11:15:46 -0400

    Fix for Bellman Ford Implementation (#651)
    * test: scipy fills with 0 but bellman ford needs Inf fill
    
    * fix: fill 0's in the diagonal
    
    * wip: remove the comment

--- benchmarks/test_julia_bellman_ford.py
+++ benchmarks/test_julia_bellman_ford.py
@@ -11,6 +11,7 @@ import pytest
 
 import numpy as np
 import scipy.io
+from scipy.sparse.csgraph import shortest_path
 
 import finch as ft
 from finch.autoschedule import COMPILE_JULIA, with_default_scheduler
@@ -42,17 +43,26 @@ def bcsstk15_graph():
     localdestpath, _ = matrix_info.download(format="MM", extract=True)
     mtx_path = Path(localdestpath) / "bcsstk15.mtx"
     matrix = scipy.io.mmread(mtx_path).tocsr()
-    return ft.asarray(matrix), matrix.shape[0]
+    matrix.data = np.abs(matrix.data)
+    matrix.data[matrix.data == 0] = 1.0
+    matrix.setdiag(0.0)
+    graph = matrix.toarray()
+    graph[graph == 0] = np.inf
+    np.fill_diagonal(graph, 0.0)
+    return ft.asarray(graph), matrix
 
 
 def test_julia_bellman_ford(bcsstk15_graph, benchmark):
-    G, n = bcsstk15_graph
+    G, matrix = bcsstk15_graph
+    n = matrix.shape[0]
     d = np.full(n, np.inf)
     d[0] = 0.0
     D = ft.asarray(d)
+    scipy_distances = shortest_path(matrix, method="BF", indices=0)
 
     with with_default_scheduler(COMPILE_JULIA):
         # We know this converges in 39 iterations on this graph
-        bellman_ford(G, D, 40, ft)
+        result = bellman_ford(G, D, 40, ft)
+        np.testing.assert_allclose(ft.to_numpy(result), scipy_distances)
 
         benchmark(bellman_ford, G, D, 40, ft)
